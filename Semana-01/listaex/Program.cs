cd295fd480c5393c615063c93f6454f8bbf92497
nando <eduardsroch@outlook.com>
2023-11-10 20:58:18 -0300

    lista de exercícios
    Iniciando a lista de exercícios

---
+++ Semana-01/listaex/Program.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// 2 Tipos de Dados:
+/// Problema: Quais são os tipos de dados numéricos inteiros disponíveis no .NET? Dê exemplos de uso para cada um deles através de exemplos.
+/// </summary>
+
+
+sbyte valorSbyte = -128; // armazena valores de -128 a 127
+Console.WriteLine(valorSbyte);
+
+byte valorByte = 255; // armazena valores de 0 a 255
+Console.WriteLine(valorByte);
+
+short valorShort = -32768; // armazena valores de -32768 a 32767
+Console.WriteLine(valorShort);
+
+ushort valorUshort = 65535; // armazena valores de 0 a 65535
+Console.WriteLine(valorUshort);
+
+int valorInt = -2147483648; // armazena valores de -2147483648 a 2147483647
+Console.WriteLine(valorInt);
+
+uint valorUint = 4294967295; // armazena valores de 0 a 4294967295
+Console.WriteLine(valorUint);
+
+long valorLong = -9223372036854775808; // armazena valores de -9223372036854775808 a 9223372036854775807
+Console.WriteLine(valorLong);
+
+ulong valorUlong = 18446744073709551615; // armazena valores de 0 a 18446744073709551615
+Console.WriteLine(valorUlong);
+
+
