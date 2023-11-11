7450aaf748af4d43555abe76c62af6031db9dcff
nando <eduardsroch@outlook.com>
2023-11-10 21:37:14 -0300

    finalizando

--- Semana-01/exercicios/Program.cs
+++ Semana-01/exercicios/Program.cs
@@ -1,8 +1,6 @@
-﻿/// <summary>
-/// 2 Tipos de Dados:
-/// Problema: Quais são os tipos de dados numéricos inteiros disponíveis no .NET? 
-/// Dê exemplos de uso para cada um deles através de exemplos.
-/// </summary>
+﻿/* 2 Tipos de Dados:
+Problema: Quais são os tipos de dados numéricos inteiros disponíveis no .NET? 
+Dê exemplos de uso para cada um deles através de exemplos.*/
 
 
 sbyte valorSbyte = -128; // armazena valores de -128 a 127
@@ -29,23 +27,20 @@ Console.WriteLine(valorLong);
 ulong valorUlong = 18446744073709551615; // armazena valores de 0 a 18446744073709551615
 Console.WriteLine(valorUlong);
 
-/// <summary>
-/// 3. Conversão de Tipos de Dados:
-//Problema: Suponha que você tenha uma variável do tipo double e deseja convertê-la 
-//em um tipo int. Como você faria essa conversão e o que aconteceria se a parte 
-//fracionária da variável double não pudesse ser convertida em um int? Resolva o 
-//problema através de um exemplo em C#.
-/// </summary>
+/* 3. Conversão de Tipos de Dados:
+Problema: Suponha que você tenha uma variável do tipo double e deseja convertê-la 
+em um tipo int. Como você faria essa conversão e o que aconteceria se a parte 
+fracionária da variável double não pudesse ser convertida em um int? Resolva o 
+problema através de um exemplo em C#.*/
+
 
 double valorDouble = 3.14;
 int valor_Int = Convert.ToInt32(valorDouble);
 Console.WriteLine(valorInt); 
 
-/// <summary>
-/// 4. Operadores Aritméticos:
-//Problema: Dada a variável int x = 10 e a variável int y = 3, escreva código para calcular 
-//e exibir o resultado da adição, subtração, multiplicação e divisão de x por y.
-/// </summary>
+/* 4. Operadores Aritméticos:
+Problema: Dada a variável int x = 10 e a variável int y = 3, escreva código para calcular 
+e exibir o resultado da adição, subtração, multiplicação e divisão de x por y.*/
 
 int x = 10;
 int y = 3;
@@ -60,10 +55,11 @@ Console.WriteLine($"Subtração: {subtracao}");
 Console.WriteLine($"Multiplicação: {multiplicacao}");
 Console.WriteLine($"Divisão: {divisao}");
 
-/// <summary>
-/// 5. Operadores de Comparação:
-//Problema: Considere as variáveis int a = 5 e int b = 8. Escreva código para determinar se a é maior que b e exiba o resultado.
-/// <summary>
+
+/* 5. Operadores de Comparação:
+Problema: Considere as variáveis int a = 5 e int b = 8. 
+Escreva código para determinar se a é maior que b e exiba o resultado.*/
+
 
 int a = 5;
 int b = 8;
@@ -77,11 +73,9 @@ else
     Console.WriteLine("b é maior que a");
 }
 
-/// <summary>
-///6. Operadores de Igualdade:
-///Problema: Você tem duas strings, string str1 = "Hello" e string str2 = "World". Escreva 
-///código para verificar se as duas strings são iguais e exibir o resultado.
-/// </summary>
+/* 6. Operadores de Igualdade:
+Problema: Você tem duas strings, string str1 = "Hello" e string str2 = "World". Escreva 
+código para verificar se as duas strings são iguais e exibir o resultado. */
 
 string str1 = "Hello";
 string str2 = "World";
@@ -95,7 +89,44 @@ else
     Console.WriteLine("As strings são diferentes");
 }
 
-/// <summary>
+
+/* 7. Operadores Lógicos:
+Problema: Suponha que você tenha duas variáveis booleanas, bool condicao1 = true 
+e bool condicao2 = false. Escreva código para verificar se ambas as condições são 
+verdadeiras e exiba o resultado.*/
+
+
+bool condicao1 = true;
+bool condicao2 = false;
+
+if (condicao1 && condicao2)
+{
+    Console.WriteLine("Ambas as condições são verdadeiras");
+}
+else
+{
+    Console.WriteLine("Pelo menos uma das condições é falsa");
+}
+
+
+/* 8. Desafio de Mistura de Operadores:
+Problema: Dadas as variáveis int num1 = 7, int num2 = 3, e int num3 = 10, escreva 
+código para verificar se num1 é maior do que num2 e se num3 é igual a num1 + num2.*/
+
+
+int num1 = 7;
+int num2 = 3;
+int num3 = 10;
+
+if (num1 > num2 && num3 == num1 + num2)
+{
+    Console.WriteLine("num1 é maior que num2 e num3 é igual a num1 + num2");
+}
+else
+{
+    Console.WriteLine("As condições não são satisfeitas");
+}
+
 
 
 
