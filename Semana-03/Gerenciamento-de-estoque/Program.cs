d7b2e1995becdf415d1cfeeb4481152502bc61f9
Eduardo S. Rocha <133709733+eduardsroch@users.noreply.github.com>
2023-11-26 18:15:03 -0300

    Correção de erros
    correção de erros no código

--- Semana-03/Gerenciamento-de-estoque/Program.cs
+++ Semana-03/Gerenciamento-de-estoque/Program.cs
@@ -4,68 +4,155 @@ namespace GerenciamentoEstoque
 {
     class Program
     {
+        static Estoque estoque = new Estoque();
+
         static void Main(string[] args)
         {
-            Estoque estoque = new Estoque();
+            bool sair = false;
 
+            while (!sair)
+            {
+                Console.WriteLine("=== MENU PRINCIPAL ===");
+                Console.WriteLine("1. Cadastro de Produtos");
+                Console.WriteLine("2. Consulta de Produtos");
+                Console.WriteLine("3. Atualização de Estoque");
+                Console.WriteLine("4. Relatórios");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha uma opção: ");
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        CadastroDeProdutos();
+                        break;
+                    case "2":
+                        ConsultaDeProdutos();
+                        break;
+                    case "3":
+                        AtualizacaoDeEstoque();
+                        break;
+                    case "4":
+                        MenuRelatorios();
+                        break;
+                    case "5":
+                        sair = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+
+        static void CadastroDeProdutos()
+        {
             try
             {
-                // Entrada de produtos
-                Console.WriteLine("Adicionar Produto 1:");
+                Console.WriteLine(" === Cadastro de Produtos === ");
                 Console.Write("Código: ");
-                int codigo1 = Convert.ToInt32(Console.ReadLine());
+                int codigo = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Nome: ");
-                string nome1 = Console.ReadLine();
+                string nome = Console.ReadLine();
                 Console.Write("Quantidade em Estoque: ");
-                int quantidade1 = Convert.ToInt32(Console.ReadLine());
+                int quantidade = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Preço Unitário: ");
-                double preco1 = Convert.ToDouble(Console.ReadLine());
+                double preco = Convert.ToDouble(Console.ReadLine());
 
-                Produto produto1 = new Produto(codigo1, nome1, quantidade1, preco1);
-                estoque.AdicionarProduto(produto1);
+                Produto produto = new Produto(codigo, nome, quantidade, preco);
+                estoque.AdicionarProduto(produto);
+                Console.WriteLine("Produto cadastrado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao cadastrar produto: {ex.Message}");
+            }
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
+        }
 
-                Console.WriteLine("Adicionar Produto 2:");
-                Console.Write("Código: ");
-                int codigo2 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Nome: ");
-                string nome2 = Console.ReadLine();
-                Console.Write("Quantidade em Estoque: ");
-                int quantidade2 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Preço Unitário: ");
-                double preco2 = Convert.ToDouble(Console.ReadLine());
-
-                Produto produto2 = new Produto(codigo2, nome2, quantidade2, preco2);
-                estoque.AdicionarProduto(produto2);
-
-                // Consulta de produto por código
+        static void ConsultaDeProdutos()
+        {
+            try
+            {
+                Console.WriteLine("=== Consulta de Produtos ===");
                 Console.Write("Buscar produto por código: ");
                 int codigoConsulta = Convert.ToInt32(Console.ReadLine());
 
                 var produtoEncontrado = estoque.BuscarProdutoPorCodigo(codigoConsulta);
                 Console.WriteLine($"Produto encontrado: {produtoEncontrado.Nome}");
-
-                // Atualização de estoque
-                Console.Write("Atualizar quantidade de um produto (Código): ");
-                int codigoAtualizacao = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Quantidade a adicionar (positivo) ou remover (negativo): ");
-                int quantidadeAtualizacao = Convert.ToInt32(Console.ReadLine());
-
-                estoque.AtualizarEstoque(codigoAtualizacao, quantidadeAtualizacao);
-
-                // Gerar relatórios
-                Console.WriteLine("Produtos abaixo de um limite de quantidade:");
-                Relatorios.GerarRelatorioProdutosAbaixoLimite(estoque, 15);
-
-                Console.WriteLine("Produtos com valor entre mínino e máximo:");
-                Relatorios.GerarRelatorioProdutosEntreValores(estoque, 5.0, 10.0);
-
-                Console.WriteLine("Relatório de valor total do estoque e valor total por produto:");
-                Relatorios.GerarRelatorioValorTotalEstoque(estoque);
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Erro: {ex.Message}");
+                Console.WriteLine($"Erro ao consultar produto: {ex.Message}");
             }
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
+        }
+
+        static void AtualizacaoDeEstoque()
+        {
+            try
+            {
+                Console.WriteLine("=== Atualização de Estoque ===");
+                Console.Write("Código do produto: ");
+                int codigoAtualizacao = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Quantidade a adicionar: ");
+                int quantidadeAtualizacao = Convert.ToInt32(Console.ReadLine());
+
+                estoque.AtualizarEstoque(codigoAtualizacao, quantidadeAtualizacao);
+                Console.WriteLine("Estoque atualizado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao atualizar estoque: {ex.Message}");
+            }
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
+        }
+
+        static void MenuRelatorios()
+        {
+            try
+            {
+                Console.WriteLine("=== Relatórios ===");
+                Console.WriteLine("1. Lista de produtos com quantidade abaixo de um limite");
+                Console.WriteLine("2. Lista de produtos com valor entre um mínimo e um máximo");
+                Console.WriteLine("3. Informar o valor total do estoque e o valor total de cada produto");
+                Console.WriteLine("4. Voltar ao menu principal");
+                Console.Write("Escolha uma opção: ");
+                string opcaoRelatorio = Console.ReadLine();
+
+                switch (opcaoRelatorio)
+                {
+                    case "1":
+                        Console.Write("Limite de quantidade: ");
+                        int limiteQuantidade = Convert.ToInt32(Console.ReadLine());
+                        Relatorios.GerarRelatorioProdutosAbaixoLimite(estoque, limiteQuantidade);
+                        break;
+                    case "2":
+                        Console.Write("Valor mínimo: ");
+                        double valorMinimo = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Valor máximo: ");
+                        double valorMaximo = Convert.ToDouble(Console.ReadLine());
+                        Relatorios.GerarRelatorioProdutosEntreValores(estoque, valorMinimo, valorMaximo);
+                        break;
+                    case "3":
+                        Relatorios.GerarRelatorioValorTotalEstoque(estoque);
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao gerar relatório: {ex.Message}");
+            }
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
         }
     }
 }
