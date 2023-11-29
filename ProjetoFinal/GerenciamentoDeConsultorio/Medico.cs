4dfeba57d0f247a10fe4a2e38774867645498002
Eduardo S. Rocha <133709733+eduardsroch@users.noreply.github.com>
2023-11-29 10:40:23 -0300

    Medico.cs
    criando a classe médico

---
+++ ProjetoFinal/GerenciamentoDeConsultorio/Medico.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciamentoDeConsultorio
+
+{
+    public class Medico
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CPF { get; set; }
+        public string CRM { get; set; }
+
+        public Medico(string nome, DateTime dataNascimento, string cpf, string crm)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            CPF = cpf;
+            CRM = crm;
+        }
+    }
+}
